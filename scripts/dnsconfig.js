b42a6cb6f8431361d8ce9df1f7e31ced108aa96c
William <172239352+willi-felix@users.noreply.github.com>
2026-02-20 14:54:57 +0700

    Update dnsconfig.js

--- scripts/dnsconfig.js
+++ scripts/dnsconfig.js
@@ -1,5 +1,7 @@
 var REG_NONE = NewRegistrar("none");
-var DNS_BIND = NewDnsProvider("gcore");
+var DNS_BIND = NewDnsProvider("gcore", {
+  "apikey": process.env["GCORE_API_KEY"]
+});
 var DOMAIN_NAME = "is-app.top"
 
 function createSubdomainsObject(jsonsPath) {
