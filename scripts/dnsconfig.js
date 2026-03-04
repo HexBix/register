c9baac23fa911b2dfb56ef65e7f25573e61a942e
Willie <172239352+williebenner@users.noreply.github.com>
2026-03-04 10:49:42 +0700

    Update dnsconfig.js

--- scripts/dnsconfig.js
+++ scripts/dnsconfig.js
@@ -1,80 +1,61 @@
 var REG_NONE = NewRegistrar("none");
-var DNS_BIND = NewDnsProvider("cloudflare");
-var DOMAIN_NAME = "is-app.top"
+var DNS_PROVIDER = NewDnsProvider("dnspod");
+var DOMAIN_NAME = "is-app.top";
 
 function createSubdomainsObject(jsonsPath) {
   var domains = [];
   try {
-    // Use glob to get JSONs name as a directory
-    // Works recursively, so subfolders are included
     var jsons = glob.apply(null, [jsonsPath, true, ".json"]);
 
-    // Push each JSON into 'domains'
     for (var i = 0; i < jsons.length; i++) {
       try {
         domains.push({ data: require(jsons[i]) });
       } catch (e) {
-        console.error(":ERROR: Error loading JSON file:", jsons[i], e);
+        console.error("Error loading JSON file:", jsons[i], e);
       }
     }
   } catch (e) {
-    console.error(":ERROR: Error finding JSON files in directory (or one of its subs) :", jsonsPath, e);
+    console.error("Error finding JSON files:", jsonsPath, e);
   }
 
   return domains;
 }
 
-// Create an object of all JSONs
-var subdomains = createSubdomainsObject('../domains')
-
+var subdomains = createSubdomainsObject('../domains');
 var records = [];
-// Parse all JSON files and generate DNS resource records for DNSControl
+
 for (var i = 0; i < subdomains.length; i++) {
-  var subdomainData = subdomains[i].data
-  var subdomain = subdomainData.subdomain
-  
-  var proxied = !!subdomainData.proxied
-  // A Records
+  var subdomainData = subdomains[i].data;
+  var subdomain = subdomainData.subdomain;
+
   if (subdomainData.records.A) {
     for (var ipv4 in subdomainData.records.A) {
-      if (proxied) {
-        records.push(A(subdomain, subdomainData.records.A[ipv4], CF_PROXY_ON));
-      } else {
-        records.push(A(subdomain, subdomainData.records.A[ipv4], CF_PROXY_OFF));
-      }
+      records.push(A(subdomain, subdomainData.records.A[ipv4]));
     }
   }
-  // AAAA Records
+
   if (subdomainData.records.AAAA) {
     for (var ipv6 in subdomainData.records.AAAA) {
-      if (proxied) {
-        records.push(AAAA(subdomain, subdomainData.records.AAAA[ipv6], CF_PROXY_ON));
-      } else {
-        records.push(AAAA(subdomain, subdomainData.records.AAAA[ipv6], CF_PROXY_OFF));
-      }
+      records.push(AAAA(subdomain, subdomainData.records.AAAA[ipv6]));
     }
   }
-  // CNAME Records
+
   if (subdomainData.records.CNAME) {
-      if (proxied) {
-        records.push(CNAME(subdomain, subdomainData.records.CNAME + ".", CF_PROXY_ON));
-      } else {
-        records.push(CNAME(subdomain, subdomainData.records.CNAME + ".", CF_PROXY_OFF));
-      }
+    records.push(CNAME(subdomain, subdomainData.records.CNAME + "."));
   }
-  // NS Records
+
   if (subdomainData.records.NS) {
     for (var ns in subdomainData.records.NS) {
       records.push(NS(subdomain, subdomainData.records.NS[ns] + "."));
     }
   }
-  // MX Records
+
   if (subdomainData.records.MX) {
     for (var mx in subdomainData.records.MX) {
       records.push(MX(subdomain, 20, subdomainData.records.MX[mx] + "."));
     }
   }
-  // TXT Records
+
   if (subdomainData.records.TXT) {
     for (var txt in subdomainData.records.TXT) {
       records.push(TXT(subdomain, subdomainData.records.TXT[txt]));
@@ -82,5 +63,4 @@ for (var i = 0; i < subdomains.length; i++) {
   }
 }
 
-// Create DNS entries using DNSControl's D() function
-D(DOMAIN_NAME, REG_NONE, DnsProvider(DNS_BIND), records);
+D(DOMAIN_NAME, REG_NONE, DnsProvider(DNS_PROVIDER), records);
